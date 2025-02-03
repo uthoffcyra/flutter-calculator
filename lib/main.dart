b51624fa65ac5eee48a48959f5d1f0055eb09293
Cyra Uthoff <uthoff.cyra@proton.me>
2025-02-03 09:52:28 -0500

    Initial Commit of the Flutter Calculator application.

---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:expressions/expressions.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Home()));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+class _HomeState extends State<Home> {
+
+  String prevexp = '';
+  String accumulator = '';
+
+  final evaluator = const ExpressionEvaluator();
+  Expression expression = Expression.parse('0');
+
+  void buttonPressed(String buttonText) {
+    if (prevexp.isNotEmpty) {
+      setState(() {
+        prevexp = '';
+      });
+    }
+
+    if (buttonText == '=') {
+      expression = Expression.parse(accumulator);
+      setState(() {
+        prevexp = accumulator;
+        accumulator = evaluator.eval(expression, {}).toString();
+      });
+
+    } else if (buttonText == 'CLEAR') {
+      setState(() {
+        accumulator = '';
+      });
+
+    } else {
+      setState(() {
+        accumulator = accumulator + buttonText;
+      });
+    }
+  }
+
+  Widget buildButton(String buttonText, {bool ?darken, bool ?notAspect}) {
+    return Expanded(
+      child: AspectRatio(
+        aspectRatio: notAspect==null ? 1.0 : 3/2,
+        child: Container(
+          margin: EdgeInsets.all(5),
+          child: FilledButton(
+            style: FilledButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.cyan[darken == null ? 400 : 700],
+            ),
+            child: Text(
+          buttonText,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+            ),
+            onPressed: () => buttonPressed(buttonText),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator', style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        )),
+        backgroundColor: Colors.cyan[700],
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.grey[900],
+        child: Column(
+          children: <Widget>[
+            const Expanded(
+              child: Text(''),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.symmetric(
+                vertical: 24,
+                horizontal: 12,
+              ),
+              child: Text(
+                prevexp.isNotEmpty ? '$prevexp = $accumulator': accumulator,
+                style: TextStyle(
+                  fontSize: 100/((accumulator.length+prevexp.length+3)*0.1 +1),
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    buildButton('7'),
+                    buildButton('8'),
+                    buildButton('9'),
+                    buildButton('/', darken: true),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buildButton('4'),
+                    buildButton('5'),
+                    buildButton('6'),
+                    buildButton('*', darken: true),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buildButton('1'),
+                    buildButton('2'),
+                    buildButton('3'),
+                    buildButton('-', darken: true),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buildButton('.'),
+                    buildButton('0'),
+                    buildButton('00'),
+                    buildButton('+', darken: true),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buildButton('CLEAR', darken: true, notAspect: true),
+                    buildButton('=', darken: true, notAspect: true),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
